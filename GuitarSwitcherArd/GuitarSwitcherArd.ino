ca31f8dd1f47ea0c683900a846a8d290775b7e57
Patrick Farley <patgadget@yahoo.ca>
2018-01-11 21:34:45 -0500

    first draft of code

---
+++ GuitarSwitcherArd/GuitarSwitcherArd.ino
@@ -0,0 +1,23 @@
+/*******************************************
+
+#define FS1 13
+#define FS2 12
+#define FS3 11
+#define FS4 10
+#define FS5 9
+#define FS6 8
+#define FS7 7
+#define FS8 6
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  
+}

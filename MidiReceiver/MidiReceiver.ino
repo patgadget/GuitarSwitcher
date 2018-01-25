7a4b076ef31336cccf89f1ac15f9a5ff25b50be9
Patrick Farley <patgadget@yahoo.ca>
2018-01-24 23:35:04 -0500

    add Define to clarify

--- MidiReceiver/MidiReceiver.ino
+++ MidiReceiver/MidiReceiver.ino
@@ -7,10 +7,21 @@
 #define pin_GREEN 5
 #define pin_BLUE 6
 #define pin_LED 13
+#define MIDI_CHANNEL 1 //1 to 16 - Midi Channel number
+#define MSG_MIDI_ACT_SENSE 0xFE // Active Sense
+#define MSG_MIDI_NOTE_ON 0x90
+#define MSG_MIDI_NOTE_OFF 0x80
+#define MSG_MIDI_POLY_KEY_PRS 0xA0 //Polyphonic Key pressure
+#define MSG_MIDI_CTRL_CHG 0xB0 // Control Change
+#define MSG_MIDI_PROG_CHG 0xC0 // Program Change
+#define MSG_MIDI_CHANNEL_PRS 0xD0 // Channel Pressure
+#define MSG_MIDI_PITCH_BEND 0xE0 // Pitch Bend
+
+
 
 
 int incomingByte =-1;
-int mainCommand=0;
+int lastValidMessage=0;
 int firstByte=-1;
 void setup()
 {
@@ -27,59 +38,38 @@ void setup()
 
 void loop()
 {    
-  //Serial.print("N");
-
   if (Serial1.available() > 0) {
     incomingByte = Serial1.read();
-    //Serial.println(incomingByte, HEX);
+    // Message MIDI car le bit 7 a ON
     if (incomingByte&0x80){
-      firstByte=-1;
-      if (incomingByte != 0xFE){
-        mainCommand=incomingByte;
-        //Serial.print("Receive mainCommmand = ");
-        //Serial.println(mainCommand,HEX);
+      firstByte=-1; //Donc ce n'est pas un DATA
+      if (incomingByte != MSG_MIDI_ACT_SENSE){ // Si ce n'est pas un ActiveSense on continue http://midi.teragonaudio.com/tech/midispec/sense.htm
+        // Est ce la Messagee 0x90 NOTE ON?
+        // et le bon Channel Midi?
+        if (!((incomingByte & 0xF0) ^ MSG_MIDI_NOTE_ON ) & !((incomingByte& 0x0F) ^ (MIDI_CHANNEL-1))){
+          lastValidMessage= (incomingByte & 0xF0); //On Garde la partie du message de gauche et on detruit le channel Midi
+        }
       }
       else{
-        mainCommand=-1;
+        // Si c'est un timecode on srap et on repar a neuf
+        lastValidMessage=-1;
       }
       
     }
     else{
-      //Serial.print("mainCommandIN:");
-      //Serial.println(mainCommand, HEX);
-      if (mainCommand > 0){
-        /*Serial.print("mainCommmand = ");
-        Serial.print(mainCommand,HEX);
-        Serial.print("Received incomingByte = ");
-        Serial.println(incomingByte, HEX);*/
-        /*if (mainCommand==0x90){
-          if (incomingByte == 0x90){
-            //Note ON,
-            mainCommand=0x90;
-            //digitalWrite(pin_LED, HIGH);
-          }
-        }
-        */
-        if (mainCommand==0x90){
+      if (lastValidMessage > 0){ //j'ai deja recu une Messagee Midi
+        if (lastValidMessage == MSG_MIDI_NOTE_ON){ //et c'est NOTE ON
           if (firstByte < 0 ){
             if (incomingByte == 0x26){
-              //Serial.println("incomingByte=0x26");
               //Note 0x26 ON,
-              //digitalWrite(pin_LED, HIGH);
               firstByte=0x26;
             }
             if (incomingByte == 0x27){
-              //Serial.println("incomingByte=0x27");
               //Note 0x27 ON
-              //digitalWrite(pin_LED, LOW);
               firstByte=0x27;
             }
           }
           else{
-            /*Serial.print("incomingByte in else = ");
-            Serial.print(incomingByte, HEX);
-            Serial.print("firstByte in else = ");
-            Serial.println(firstByte, HEX);*/
             if (firstByte == 0x26 ){
               if (incomingByte | 1){ // Any Velocity
                 digitalWrite(pin_LED, HIGH);
@@ -95,16 +85,7 @@ void loop()
         }
       }
     }
-    //Serial.print("mainCommandOUT:");
-    //Serial.println(mainCommand, HEX);
-
   }
-     
-  //digitalWrite(pin_LED, HIGH);
-  //Serial.print("Code: ");
-  //Serial.println(incomingByte, HEX);
-    
-  
   //analogWrite(pin_RED,0);
   //analogWrite(pin_GREEN,0);
   //analogWrite(pin_BLUE,0);
